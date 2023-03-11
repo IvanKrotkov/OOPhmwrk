fd4fd1f8f2bc37ba7d141941cb8124eb2597ce57
Ivan <csgo.dr@mail.ru>
2023-03-11 16:14:57 +0700

    update Tiger

--- Seminar2/Tiger.java
+++ Seminar2/Tiger.java
@@ -1,13 +1,22 @@
 package Seminar2;
 
+import java.util.Date;
+
 public class Tiger extends WildAnimal implements MakeASound{
+    public Tiger(String habitat, Date dateOfFinding, int weight, int height, String eyeColor){
+        super(habitat, dateOfFinding, weight, height, eyeColor);
+    }
+    public Tiger(){
+        this("тропический лес",new Date(),110,115,"зелёный");
+    }
     @Override
     public void makeASound() {
-
+        System.out.println("ар-р-н-х");
     }
-
     @Override
     public String toString() {
-        return super.toString();
+        StringBuilder str = new StringBuilder(super.toString());
+        str.insert(17,"тигр, ");
+        return str.toString();
     }
 }
