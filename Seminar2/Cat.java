22c6689cd4b80c23699607c971badd4d617e5b95
Ivan <csgo.dr@mail.ru>
2023-03-11 15:52:41 +0700

    add class cat

---
+++ Seminar2/Cat.java
@@ -0,0 +1,12 @@
+package Seminar2;
+
+public class Cat extends Pet implements MakeASound{
+    private Boolean thePresenceOfWool;
+    @Override
+    public void caress() {
+    }
+    @Override
+    public void makeASound() {
+
+    }
+}
