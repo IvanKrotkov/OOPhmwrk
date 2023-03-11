d126756814ff030cc3677d6d1237732f82de0bf4
Ivan <csgo.dr@mail.ru>
2023-03-11 15:53:37 +0700

    add class Tiger

---
+++ Seminar2/Tiger.java
@@ -0,0 +1,13 @@
+package Seminar2;
+
+public class Tiger extends WildAnimal implements MakeASound{
+    @Override
+    public void makeASound() {
+
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
