c3d7e46e4cc1dfadbd4b751f784dc7a7824aa5cd
Ivan <csgo.dr@mail.ru>
2023-03-11 16:47:19 +0700

    optimize class cat and Pet

--- Seminar2/Cat.java
+++ Seminar2/Cat.java
@@ -1,12 +1,38 @@
 package Seminar2;
 
 public class Cat extends Pet implements MakeASound{
-    private Boolean thePresenceOfWool;
+    enum thePresenceOfWool{
+        bald,withWool;
+    }
+    private thePresenceOfWool wooliness;
+    public Cat(thePresenceOfWool wooliness,String name, String breed, AvailabilityOfVaccinations availability, String woolColor, int weight, int height, String eyeColor) {
+        super(name, breed, availability, woolColor, weight, height, eyeColor);
+        this.wooliness = wooliness;
+    }
+    public Cat(thePresenceOfWool wooliness){
+        this(wooliness,"Барсик","Сиамская", AvailabilityOfVaccinations.grafted,"серый",4,23,"голубые");
+    }
+    public Cat(){
+        this(thePresenceOfWool.withWool);
+    }
     @Override
     public void caress() {
+        System.out.println("Мурлычит и трётся о ноги");
     }
     @Override
     public void makeASound() {
+        System.out.println("мяууу");
+    }
 
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder(super.toString());
+        str.insert(20,"кошка, ");
+        str.append("наличие шерсти: " + wooliness);
+        return str.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Cat());
     }
 }
