ef2c6b8885d7daaeee011e5ba0f0f5e54d159664
Ivan <csgo.dr@mail.ru>
2023-03-11 16:47:42 +0700

    optimize class Pet

--- Seminar2/Pet.java
+++ Seminar2/Pet.java
@@ -3,12 +3,26 @@ package Seminar2;
 public abstract class Pet extends Animal{
     protected String name;
     protected String breed;
-    protected Boolean availabilityOfVaccinations;
-    protected String coatColor;
+    enum AvailabilityOfVaccinations{
+        grafted,unvaccinate;
+    }
+    private AvailabilityOfVaccinations availability;
+    protected String woolColor;
     public abstract void caress();
+    public Pet(String name, String breed,AvailabilityOfVaccinations availability, String woolColor,int weight, int height, String eyeColor){
+        super(weight, height, eyeColor);
+        this.name = name;
+        this.breed = breed;
+        this.availability = availability;
+        this.woolColor = woolColor;
+    }
 
     @Override
     public String toString() {
-        return "Домашнее животное";
+        StringBuilder str = new StringBuilder(super.toString());
+        str.insert(0,"Домашнее животное - ");
+        str.append(String.format("кличка: %s, порода: %s, цвет шерсти: %s, ",name,breed,woolColor));
+        str.append("наличие прививок: " + availability + ", ");
+        return str.toString();
     }
 }
